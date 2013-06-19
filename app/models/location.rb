275f0f64206c810505de20f489c860b6873754b3
Amy Erz <wingmerz@gmail.com>
2013-06-19 09:07:29 -0700

    added views

--- app/models/location.rb
+++ app/models/location.rb
@@ -2,5 +2,5 @@ class Location < ActiveRecord::Base
   has_and_belongs_to_many :movies
   attr_accessible :address, :fun_fact  #I removed movie_id because I created a bridge table between locations and movies
   geocoded_by :address
-  after_validation :geocode 
+  after_validation :geocode, :if => :address_changed?
 end
