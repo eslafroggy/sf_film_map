3d368d1554efe386c16aac344ce3471260e91b40
eslafroggy <claire.pierce@gmail.com>
2013-06-19 10:34:26 -0700

    fixed views so locations show up

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,6 +1,8 @@
 class LocationsController < ApplicationController
 
   def index
+    @locations = Location.all
+    
     respond_to do |format|
       format.html # index.html.erb
       format.json { render json: @locations }
