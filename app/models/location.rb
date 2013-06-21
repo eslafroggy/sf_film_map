30fe2ba86e1c9dba5b2cf996f701ab9c59a029ec
Amy Erz <wingmerz@gmail.com>
2013-06-20 17:38:05 -0700

    update the update location function due to geocode not working perfectly

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,13 +1,13 @@
 class Location < ActiveRecord::Base
   has_and_belongs_to_many :movies
-  attr_accessible :address, :fun_fact  #I removed movie_id because I created a bridge table between locations and movies
+  attr_accessible :address, :fun_fact, :longitude, :latitude#I removed movie_id because I created a bridge table between locations and movies
   geocoded_by :address
   after_validation :geocode, :if => :address_changed?
 
-  acts_as_gmappable
+  #acts_as_gmappable
 
-  def gmaps4rails_address
-    self.address
-  end
+  #def gmaps4rails_address
+   # self.address
+  #end
 
 end
