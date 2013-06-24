5834850eec37611cca52fdc57f0b2cc7d9bcc77c
Amy Erz <wingmerz@gmail.com>
2013-06-23 18:50:43 -0700

    updated relationship for new locations and movie

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,6 +1,6 @@
 class Location < ActiveRecord::Base
   has_and_belongs_to_many :movies
-  attr_accessible :address, :fun_fact, :longitude, :latitude #I removed movie_id because I created a bridge table between locations and movies
+  attr_accessible :address, :fun_fact, :longitude, :latitude, :movie_ids #I removed movie_id because I created a bridge table between locations and movies
   geocoded_by :address
   after_validation :geocode, :if => :address_changed?
 
